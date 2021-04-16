3f54559916795bd19a4cbd8b9b80300ad6f89e5f
Vrund Soni <59587177+vrundsoni0032@users.noreply.github.com>
2021-04-16 02:51:04 -0400

    fixed previous commit

--- ColdNites/Source/AG_ColdNites/SaveAndLoad/AG_ColdNitesSaveGame.h
+++ ColdNites/Source/AG_ColdNites/SaveAndLoad/AG_ColdNitesSaveGame.h
@@ -14,6 +14,8 @@
 #include "GameFramework/SaveGame.h"
 #include "AG_ColdNitesSaveGame.generated.h"
 
+enum class AG_StarType : unsigned char;
+
 UCLASS()
 class AG_COLDNITES_API UAG_ColdNitesSaveGame : public USaveGame
 {
@@ -24,9 +26,13 @@ public:
 
     UPROPERTY(VisibleAnywhere, Category = "AG_SaveGame")
         TArray<int> CompletedLevels;
-
-    UPROPERTY(VisibleAnywhere, Category = "AG_SaveGame")
-        TMultiMap<FString, AG_StarType> CollectedLevelStars;
+    
+    //NOTE : The Current Unreal Engine 4.26 does not support UPROPERTY Macro for TMultiMap Yet -
+    //  https://answers.unrealengine.com/questions/588193/tmultimap-not-allowed-as-a-uproperty.html
+    // As a result, CollectedTotalStars will not work for the new Instance of the Game and will be reseted.
+    // Though CollectedTotalStars will work perfectly with the same running Instance of the Game.
+    //UPROPERTY(VisibleAnywhere, Category = "AG_SaveGame")    
+        TMultiMap<FString, AG_StarType> CollectedTotalStars;
 
     UPROPERTY(VisibleAnywhere, Category = "AG_SaveGame")
         FString SaveSlotName;
